0a59e9a928a5280624c6905d7f4a43cbc96bfaa0
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-04-13 20:14:56 +0530

    instanceof Operator

--- src/InstanceOperator/instanceofoperator.java
+++ src/InstanceOperator/instanceofoperator.java
@@ -1,4 +1,21 @@
 package InstanceOperator;
+class book
+{
+    void show()
+    {
+        System.out.println("Is a Java Books...");
+    }
+}
+class  pan extends book
+{}
+public class instanceofoperator extends pan
+{
+    public static void main(String[] args)
+    {
+        System.out.println("----Output------");
+        instanceofoperator i1=new instanceofoperator();
+        System.out.println(i1 instanceof book);
+        i1.show();
+    }
 
-public class instanceofoperator {
 }
