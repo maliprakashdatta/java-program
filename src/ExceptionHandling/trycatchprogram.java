7d16c235addd4c7242b2511847356dfad9db6b99
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-04-15 20:57:05 +0530

    try catch block

---
+++ src/ExceptionHandling/trycatchprogram.java
@@ -0,0 +1,20 @@
+package ExceptionHandling;
+
+public class trycatchprogram
+{
+    public static void main(String[] args)
+    {
+        try {
+            int Number=50/0; //This is throw exception
+
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("Rest of the code");
+    }
+
+
+
+}
