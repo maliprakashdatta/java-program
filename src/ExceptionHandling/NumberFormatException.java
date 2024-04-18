ff4071531f7413ca941eb5a9c9e9010181c27b8f
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-04-18 21:59:05 +0530

    NumberFormatException Program

---
+++ src/ExceptionHandling/NumberFormatException.java
@@ -0,0 +1,21 @@
+package ExceptionHandling;
+
+public class NumberFormatException
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Check the number Format Exception");
+        String Str="8830";
+                try
+                {
+                    int n=Integer.parseInt(Str);
+                    System.out.println(n);
+                }
+             catch(Exception n)
+             {
+                 //System.out.println("Is Cant"+n+" Conver the Char to int");
+                 System.out.println(n);
+             }
+        System.out.println("Main method is ended..");
+    }
+}
