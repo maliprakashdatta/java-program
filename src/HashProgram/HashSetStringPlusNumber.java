c89a204ccc292ea5f51d60c73b63c20fd86ea7a1
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-04-27 15:25:07 +0530

    Hash Map String Integer

--- src/HashProgram/HashSetStringPlusNumber.java
+++ src/HashProgram/HashSetStringPlusNumber.java
@@ -1,43 +1,25 @@
 // Java program to illustrate HashMap class
 // of java.util package
 
-// Importing HashMap class
+// Importing Has
 import java.util.HashMap;
 
-// Main class
-public class HashSetStringPlusNumber {
+public class HashSetStringPlusNumber
+{
 
-    // Main driver method
     public static void main(String[] args)
     {
-        // Create an empty hash map by declaring object
-        // of string and integer type
         HashMap<String, Integer> map = new HashMap<>();
-
-        // Adding elements to the Map
-        // using standard put() method
-        map.put("vishal", 10);
-        map.put("sachin", 30);
+        map.put("Prakash", 33);
+        map.put("sachin", 34);
         map.put("vaibhav", 20);
-
-        // Print size and content of the Map
-        System.out.println("Size of map is:- "
-                + map.size());
-
-        // Printing elements in object of Map
+        System.out.println("Size of map is:- " + map.size());
         System.out.println(map);
-
-        // Checking if a key is present and if
-        // present, print value by passing
-        // random element
-        if (map.containsKey("vishal")) {
-
-            // Mapping
-            Integer a = map.get("vishal");
-
-            // Printing value for the corresponding key
-            System.out.println("value for key"
-                    + " \"vishal\" is:- " + a);
-        }
+        if (map.containsKey("Prakash"))
+          {
+            Integer a = map.get("Prakash");
+            System.out.println("value for key" + " \"Prakash\" is:- " + a);
+          }
+        System.out.println("Size of set is:"+map.size());
     }
 }
