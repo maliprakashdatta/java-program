cd0e5d7601c4318db7a8399a2d2a3954419d5fd1
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-02 22:57:09 +0530

    ......

---
+++ src/HomeWork/ProgramNineteen19.java
@@ -0,0 +1,10 @@
+//Write a program to calculate HCF of Two given number.
+package HomeWork;
+public class ProgramNineteen19
+{
+    public static void main(String[] args)
+    {
+        //not understand program logic
+
+    }
+}
