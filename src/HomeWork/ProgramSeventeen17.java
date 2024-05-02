bd911e85e2b4174ab670938ae104ba4ebed43317
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-02 22:56:39 +0530

    ......

---
+++ src/HomeWork/ProgramSeventeen17.java
@@ -0,0 +1,9 @@
+//Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+// Calculate the Sum of First N Odd & Even Numbers in Java
+package HomeWork;
+public class ProgramSeventeen17
+{
+    public static void main(String[] args) {
+       //not understand program logic
+    }
+}
