84b4fabe16022a1cff8602f9491662f244cb1ff9
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-09 20:24:08 +0530

    ArrayList Program

--- src/Collection/ArrayListDemo1.java
+++ src/Collection/ArrayListDemo1.java
@@ -12,9 +12,22 @@ public class ArrayListDemo1
         list.add(4);
         list.add(5);
         System.out.println(list);
+
+        //add Number
+        list.set(2,3);
+        System.out.println("Set element:-"+list);
+        //Contain Elemant
+        list.contains(0);
+        System.out.println("Contain the elemants"+list);
+        //Array Size
+        list.size();
+        System.out.println("Array Size:-"+list);
         //remove arraylist
         list.remove(2);
         System.out.println("Remove the arraylist"+list);
+        //Set Size
+        list.set(2,6);
+        System.out.println("Set The Element:-"+list);
 
     }
 
