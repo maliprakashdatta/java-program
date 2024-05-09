b7884ec900d2fee39c9f2a8345bd5982280fc599
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-09 20:34:34 +0530

    ArrayList String Program

---
+++ src/Collection/ArrayListString.java
@@ -0,0 +1,15 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class ArrayListString {
+    public static void main(String[] args) {
+        ArrayList list=new ArrayList();
+        list.add("Prakash");
+        list.add("Datta");
+        list.add("Mali");
+        list.add("Jath");
+        list.add("RRC");
+        System.out.println(list);
+    }
+}
