cced09a9023019a6a08bbb0489ba36467a87db09
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-10 07:44:41 +0530

    LinkedList Program

---
+++ src/Collection/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package Collection;
+import java.util.LinkedList;
+public class LinkedListDemo
+{
+    public static void main(String[] args)
+    {
+
+        LinkedList list=new LinkedList();
+        list.add("Prakash");
+        list.add("Datta");
+        list.add("Mali");
+        list.add("Jath");
+        list.add("RRC");
+        System.out.println(list);
+
+    }
+}
