b830e6cdaed0f02be6be94936935a3680aede60c
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-10 08:09:12 +0530

    ArrayList String

--- src/Collection/ArrayListDemo1.java
+++ src/Collection/ArrayListDemo1.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class ArrayListDemo1
 {
     public static void main(String[] args) {
-        ArrayList list=new ArrayList();
+        ArrayList<Integer> list=new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
