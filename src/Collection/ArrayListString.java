bdfb0768f54e0440040c984a840bd85bbbd0fe63
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-10 08:09:33 +0530

    ArrayList String

--- src/Collection/ArrayListString.java
+++ src/Collection/ArrayListString.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class ArrayListString {
     public static void main(String[] args) {
-        ArrayList list=new ArrayList();
+        ArrayList<String> list=new ArrayList<>();
         list.add("Prakash");
         list.add("Datta");
         list.add("Mali");
         list.add("Jath");
         list.add("RRC");
-        System.out.println(list);
+        System.out.println("ArrayList :-"+list);
     }
 }
