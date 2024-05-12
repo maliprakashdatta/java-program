4c4080a62bb2e407e9161967509134afa6014b0d
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-12 18:35:45 +0530

    to append the specified element to the end of a hash set

---
+++ src/Homework_collection_HashSet/Program1.java
@@ -0,0 +1,19 @@
+//Write a Java program to append the specified element to the end of a hash set.
+package Homework_collection_HashSet;
+
+import java.util.HashSet;
+
+public class Program1 {
+    public static void main(String[] args) {
+        //Creat the HashSet
+        HashSet<String> Hash_set=new HashSet<>();
+        Hash_set.add("Fan");
+        Hash_set.add("TV");
+        Hash_set.add("Laptop");
+        Hash_set.add("AC");
+        Hash_set.add("Mobile");
+        System.out.println("To Display Electrical Hash_set:-"+Hash_set);
+
+
+    }
+}
