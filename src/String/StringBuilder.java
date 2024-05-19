78c78db89b3920e1a64e8db654eddd433e9d3848
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-19 18:21:33 +0530

    StringBuilder program

---
+++ src/String/StringBuilder.java
@@ -0,0 +1,12 @@
+package String;
+
+public class StringBuilder {
+    public static void main(String[] args)
+    {
+
+                java.lang.StringBuilder builderCity = new java.lang.StringBuilder();
+                builderCity.append("Jath Sangli");
+                System.out.println("builder city: " + builderCity);
+                System.out.println("builder city.capacity(): " + builderCity.capacity());
+    }
+}
