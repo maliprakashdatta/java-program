0364b48d3c7b472c67ccebf1c02b8262e1fa43c7
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 15:10:27 +0530

    trim String

---
+++ src/String/trimString.java
@@ -0,0 +1,8 @@
+package String;
+
+public class trimString {
+    public static void main(String[] args) {
+        String s1="Prakash";
+        System.out.println("Printed String:-"+s1.trim());//No Space for Frant
+    }
+}
