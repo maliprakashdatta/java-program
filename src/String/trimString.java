846291de03116f4559030e2902469fe811ac37f6
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 15:25:58 +0530

    trim of Program

--- src/String/trimString.java
+++ src/String/trimString.java
@@ -3,7 +3,7 @@ package String;
 public class trimString {
     public static void main(String[] args) {
         String s1="Prakash";
-        System.out.println("Printed String:-"+s1.trim());//No Space for Frant
+        System.out.println("Printed String:-"+s1.trim());//No Space for frant
 
     }
 }
