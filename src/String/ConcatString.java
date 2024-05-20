e1ee29dee48fc0e82fe4a246f64a7ed5d91a6ed3
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 14:48:39 +0530

    Two String Concat by Objects

---
+++ src/String/ConcatString.java
@@ -0,0 +1,14 @@
+package String;
+
+public class ConcatString {
+    public static void main(String[] args) {
+        //To add to word concat
+        String s1="Prakash Datta";
+        s1=s1.concat(" Mali ");
+        System.out.println("Printed String:-"+s1);
+        //Without Ref s1=s1.concat();
+//        String s1="Prakash Datta";
+//        s1.concat(" Mali ");
+//        System.out.println("Printed String:-"+s1);
+    }
+}
