3758a9633a0133acf64d64b3e46e7069a6fc8679
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 14:18:32 +0530

    Using new keyword String

---
+++ src/String/Bynewkeywordstring.java
@@ -0,0 +1,9 @@
+package String;
+
+public class Bynewkeywordstring {
+    public static void main(String[] args)
+    {
+        String s=new String("MaliPrakash");
+        System.out.println("My String :-"+s);
+    }
+}
