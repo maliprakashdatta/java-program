e8d2a7bfedbf140ea9d873fa95c0c0308d440def
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 15:22:08 +0530

    StartsWith and EndsWith String

---
+++ src/String/StartsWithAndEndsWith.java
@@ -0,0 +1,12 @@
+package String;
+
+public class StartsWithAndEndsWith {
+    public static void main(String[] args) {
+        String s1="Prakash";
+        //To check the firts two Char is check
+        System.out.println("StartsWith  String T/F:-"+s1.startsWith("Pr"));
+        //To check the Last two Char is check
+        System.out.println("EndsWith  String T/F:-"+s1.endsWith("sd"));
+
+    }
+}
