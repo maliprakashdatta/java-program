7e78bfc99ea5e7262b50e1c620f1c22ac43fa0af
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 15:15:17 +0530

    toUperAndLowerCaseString String

---
+++ src/String/toUperAndLowerCaseString.java
@@ -0,0 +1,9 @@
+package String;
+
+public class toUperAndLowerCaseString {
+    public static void main(String[] args) {
+        String s1="StepUp Learn pune  ";
+        System.out.println("toUpperCase is String:-"+s1.toUpperCase());//toUppercase
+        System.out.println("toLowerCase is String:-"+s1.toLowerCase());//toLowerCase
+    }
+}
