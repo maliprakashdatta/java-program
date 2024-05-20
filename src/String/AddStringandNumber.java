cd6f329867d881fd99b049b4d84b6edb717c99f0
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 15:05:10 +0530

    Number + Char in String

---
+++ src/String/AddStringandNumber.java
@@ -0,0 +1,10 @@
+package String;
+
+public class AddStringandNumber {
+    public static void main(String[] args) {
+        //Add Number and Char My String
+        String str="\nPrakash Mali Vahical Info"+"\n------------------------"+"\nBike Number:-"+ 5025 +"\nCar Number:-"+ 3216;
+        System.out.println("Add Number and Char My String :-"+str);
+    }
+    }
+
