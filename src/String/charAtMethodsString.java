ef17462ac469ee2060f41ec234d11610426a9cca
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 15:31:01 +0530

    the address number of char

---
+++ src/String/charAtMethodsString.java
@@ -0,0 +1,11 @@
+package String;
+
+public class charAtMethodsString {
+    public static void main(String[] args) {
+        String s1="JavaDeveloper";
+        //CharAt() in String check the address number of char
+        System.out.println("CharAt() in String:-"+s1.charAt(0));
+        System.out.println("CharAt() in String:-"+s1.charAt(4));
+
+    }
+}
