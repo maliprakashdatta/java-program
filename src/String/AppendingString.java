71b15ea6f1db52dbff54280cb19fe79a9b60ac4a
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 14:53:59 +0530

    By+ Operator String

---
+++ src/String/AppendingString.java
@@ -0,0 +1,9 @@
+package String;
+public class AppendingString {
+    public static void main(String[] args) {
+        String s1="Prakash Datta"+" Mali";
+        //s1=s1.concat(" Mali ");
+        System.out.println("Printed String:-"+s1);
+    }
+
+}
