cd720d05981ce6745445a0b3c1e21ee069083b2b
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-05-20 15:24:46 +0530

    To check the Length of String

---
+++ src/String/LengthOfString.java
@@ -0,0 +1,10 @@
+package String;
+
+public class LengthOfString {
+    public static void main(String[] args) {
+        String s1="FullStack Java Developer";
+        //To check the Length of String
+        System.out.println("Length of String:-"+s1.length());
+    }
+
+}
