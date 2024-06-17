8eb12e23854ae98c3b236f9c38c0368bb02af402
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-06-17 18:39:22 +0530

    File Creating Program

---
+++ src/FileHandling/DemoFile.java
@@ -0,0 +1,13 @@
+package FileHandling;// Importing File Class
+import java.io.File;
+
+class DemoFile
+{
+    public static void main(String[] args)
+    {
+
+        // File name specified
+        File obj = new File("myfile.txt");
+        System.out.println("File Created!");
+    }
+}
