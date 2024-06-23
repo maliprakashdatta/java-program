d1e008e0287bac9f9d27650ecb9de9d71a51a9cb
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-06-23 12:04:03 +0530

    ArrayList Lambda Program

---
+++ src/LambdaExpressions/LambdaArrayList.java
@@ -0,0 +1,16 @@
+package LambdaExpressions;
+
+import java.util.ArrayList;
+
+public class LambdaArrayList {
+
+        public static void main(String[] args) {
+            ArrayList<Integer> numbers = new ArrayList<Integer>();
+            numbers.add(5);
+            numbers.add(9);
+            numbers.add(8);
+            numbers.add(1);
+            numbers.forEach( (n) -> { System.out.println("Value of ArrayList:-"+n); } );
+        }
+    }
+
