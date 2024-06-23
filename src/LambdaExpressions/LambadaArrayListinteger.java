9443883a64609f614d1616282a53ddd682b889bc
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-06-23 12:03:15 +0530

    Lambda Program

---
+++ src/LambdaExpressions/LambadaArrayListinteger.java
@@ -0,0 +1,19 @@
+package LambdaExpressions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LambadaArrayListinteger {
+    public static void main(String[] args) {
+    ArrayList<Integer> Number=new ArrayList<>();
+        Number.add(1);
+        Number.add(3);
+        Number.add(2);
+        Number.add(5);
+        Number.add(4);
+        System.out.println("Orignal ArrayList:-"+Number);
+    //to sort a given array list
+        Collections.sort(Number);
+        System.out.println("To sorted a given array list"+Number);
+    }
+}
