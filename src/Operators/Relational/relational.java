ccb48029b28ba5f0480541f2e77942b26170e3d1
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-07-13 22:16:43 +0530

    Relational Operators Program

---
+++ src/Operators/Relational/relational.java
@@ -0,0 +1,23 @@
+//java includes six relational operators:
+//        == (equal to)
+//        != (not equal to)
+//        < (less than)
+//        > (greater than)
+//        <= (less than or equal to)
+//        >= (greater than or equal to)
+package Operators.Relational;
+
+public class relational {
+    public static void main(String[] args) {
+        int a=10;
+        int b=4;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+
+
+    }
+}
